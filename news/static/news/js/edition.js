06aa0d261866646104dea948817124d8e6fe2d93
Ronie Salgado <roniesalg@gmail.com>
2014-10-10 22:18:51 -0300

    Basic event form.

--- news/static/news/js/edition.js
+++ news/static/news/js/edition.js
@@ -15,16 +15,48 @@ $(document).ready(function() {
             $( "#leftPanelAccordion" ).accordion( "refresh" );
         }
     });
+
+    $( ".dateInput" ).datepicker({
+        dateFormat: 'dd-mm-yy',
+        
+    });
 });
 
 // Update the current alerts.
 function refreshAlerts()
 {
-    //console.log('refreshAlerts');
+    $.getJSON(CurrentAlertsURL, function(alerts) {
+        // Get the alerts block
+        var alertsBlock = $("#alertsBlock");
+        alertsBlock.empty();
+
+        // Display a no alerts message if there is none.
+        if(alerts.length == 0)
+        {
+            alertsBlock.html("No hay alertas");
+            return;
+        }
+
+        // Create the alerts table.
+        var alertsTable = $('<table></table>');
+        alertsBlock.append(alertsTable);
+
+        // Read the alerts.
+        for(var i = 0; i < alerts.length; ++i) {
+            var alert = alerts[i];
+            var id = alert.pk;
+            var fields = alert.fields;
+
+            // Add the row.
+            var row = $('<tr><td>' + fields.message + '</td></tr>');
+            alertsTable.append(row);
+        }
+    });
 }
 
 // Setup some periodic functions.
 $(window).load(function() {
     // Update alerts
+    refreshAlerts();
     window.setInterval(refreshAlerts, AlertRefreshTime);
 });
